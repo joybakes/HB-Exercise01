7c7096a8e173b97a1a3c2b41d212eef243db87c0
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 12:36:41 -0800

    imported random and set answer to random.randomint. woo! now we're pretty much ready write more.

--- guess.py
+++ guess.py
@@ -1,4 +1,7 @@
+import random
+
 name = raw_input("Welcome. Enter your name here: ")
 print "Ok, %s, I'm thinking of a number between 1 and 100." % name 
 print "Keep entering a guess, I'll give you hints to tell you if your high or low"
 guess = int(raw_input("Guess a number: "))
+answer = random.randint(0,101)
