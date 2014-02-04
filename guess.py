8670f62bb06255cd2b30e6d16a302ff533281924
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 15:50:26 -0800

    this is our finished number guesser! now with improved error checking features. break that! (actually pls dont)

--- guess.py
+++ guess.py
@@ -5,17 +5,24 @@ print "Ok, %s, I'm thinking of a number between 1 and 100." % name
 print "Keep entering a guess, I'll give you hints to tell you if your high or low"
 answer = random.randint(0,101)
 tries = 1
-guess = int(raw_input("Guess a number: "))
 
-while tries >= 1:
-    if guess > answer:
-        print "Your guess is too high, try again."
-        guess = int(raw_input("Guess a number: "))
-        tries += 1
-    elif guess < answer:
-        print "Your guess is too low, try again."
-        guess = int(raw_input("Guess a number: "))
-        tries += 1
+
+while True:
+    guess = raw_input("Guess a number: ")
+    print answer
+    if guess.isdigit() == True:
+        guess = int(guess)    
+        if guess in range(0,101):
+            if guess > answer:
+                print "Your guess is too high, try again."
+                tries += 1
+            elif guess < answer:
+                print "Your guess is too low, try again."
+                tries += 1
+            else:
+                print "Good job %s! You guessed correctly in %d tries! You win!" % (name,tries)
+                break
+        else:
+            print "You're not funny. Follow the rules."
     else:
-        print "Good job %s! You guessed correctly in %d tries! You win!" % (name,tries)
-        break
+        print "You're not funny. Follow the rules."
