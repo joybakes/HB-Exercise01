47a74ae80500f11f58c4303ba1c708bd05726ec8
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 12:44:23 -0800

    added if/else statement (or the beginning of it atleast) and first tries increment counter at 0)

--- guess.py
+++ guess.py
@@ -5,3 +5,15 @@ print "Ok, %s, I'm thinking of a number between 1 and 100." % name
 print "Keep entering a guess, I'll give you hints to tell you if your high or low"
 guess = int(raw_input("Guess a number: "))
 answer = random.randint(0,101)
+tries = 0
+
+if guess == answer:
+    print "Good job %s! You guessed correctly in %s tries! You win!" (name, tries)
+
+elif guess > answer:
+    print "Your guess is too high, try again."
+
+elif guess < answer:
+    print "Your guess is too low, try again."
+
+ 
