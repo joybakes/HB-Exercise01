1678eb20d3b563966f825000f2bcd623c0b0abbd
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 12:27:20 -0800

    first commit. incl game intro, username, and prompt for first guess

---
+++ guess.py
@@ -0,0 +1,4 @@
+name = raw_input("Welcome. Enter your name here: ")
+print "Ok, %s, I'm thinking of a number between 1 and 100." % name 
+print "Keep entering a guess, I'll give you hints to tell you if your high or low"
+guess = int(raw_input("Guess a number: "))
